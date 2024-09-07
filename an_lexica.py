8506285d8dd4facfc062699058507dc1dfe512bc
povaloid <rc.jorgeaugusto@gmail.com>
2024-09-06 23:50:45 -0300

    Adiciona arquivos iniciais do compilador.

---
+++ an_lexica.py
@@ -0,0 +1,9 @@
+class palavras_reservadas:
+    # Define tipos de dados possíveis
+    TIPO_DE_DADO: []
+
+    # Define operadores aritméticos
+    OPERADORES_ARITMETICOS: []
+
+    # Define operadores lógicos
+    OPERADORES_LOGICOS: []
